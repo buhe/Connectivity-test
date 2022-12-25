f6e7f45287b9b4cc4ec2edf389c4f7e772760c7c
晒晕的兔子 <gubu1986@126.com>
2022-12-25 19:14:41 +0800

    Initial Commit

---
+++ Connectivity test/Connectivity_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Connectivity_testApp.swift
+//  Connectivity test
+//
+//  Created by 顾艳华 on 2022/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Connectivity_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
